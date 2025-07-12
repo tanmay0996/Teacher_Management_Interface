022184d7a293c81560e7b64963fd0e9b7f478509
tanmay0996 <167513949+tanmay0996@users.noreply.github.com>
2025-07-12 17:24:49 +0530

    toast fix

--- components/ui/toaster.tsx
+++ components/ui/toaster.tsx
@@ -1,35 +1,10 @@
 'use client';
 
 import { useToast } from '@/hooks/use-toast';
-import {
-  Toast,
-  ToastClose,
-  ToastDescription,
-  ToastProvider,
-  ToastTitle,
-  ToastViewport,
-} from '@/components/ui/toast';
+import { Toaster as CustomToaster } from '@/components/ui/toast'; // Import your own Toaster
 
 export function Toaster() {
-  const { toasts } = useToast();
+  const { toasts, dismiss } = useToast();
 
-  return (
-    <ToastProvider>
-      {toasts.map(function ({ id, title, description, action, ...props }) {
-        return (
-          <Toast key={id} {...props}>
-            <div className="grid gap-1">
-              {title && <ToastTitle>{title}</ToastTitle>}
-              {description && (
-                <ToastDescription>{description}</ToastDescription>
-              )}
-            </div>
-            {action}
-            <ToastClose />
-          </Toast>
-        );
-      })}
-      <ToastViewport />
-    </ToastProvider>
-  );
+  return <CustomToaster toasts={toasts} onDismiss={dismiss} />;
 }
